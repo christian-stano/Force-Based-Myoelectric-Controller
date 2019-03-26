e67cf32c97680e50f70d65a842c820bb2040f641
king_ally <allyson.r.king@vanderbilt.edu>
2019-03-26 17:06:35 -0500

    hi

--- Serial_Output_EMG/sketch_feb17a/sketch_feb17a.ino
+++ Serial_Output_EMG/sketch_feb17a/sketch_feb17a.ino
@@ -4,22 +4,27 @@
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
+ 
   Serial.begin(9600);
+   Serial.println("LABEL,Time, MuscleA0,MuscleA1");
+   Serial.println("RESETTIMER"); //resets timer to 0
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
-  //int sensorValue2 = analogRead (A1);
+  int sensorValue2 = analogRead (A1);
 
-  double outputfinal= analogRead(A2); 
+  //double outputfinal= analogRead(A2); 
   // print out the value you read:
-    Serial.println(sensorValue);
-    Serial.print(" ");   
-    Serial.print(",");              //seperator
-    Serial.print(" ");   
-    //Serial.println(sensorValue2);          //the second variable for plottingincluding line break
+    Serial.print("DATA, TIME,"); 
+    Serial.print(sensorValue);
+    Serial.print(" ");
+    Serial.print(",");
+    Serial.print(" ");
+    Serial.println(sensorValue2);   //the second variable for plottingincluding line break
+
     
-  delay(1);        // delay in between reads for stability
+  delay(10);        // delay in between reads for stability
 }
